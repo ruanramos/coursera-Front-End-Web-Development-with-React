dcab46d626ef6e3fdb70267f367479b416530bfe
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-07-07 18:36:12 -0300

    Add react router, header and footer

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import Main from './components/MainComponent';
-import { DISHES } from './shared/dishes';
+import React, { Component } from "react";
+import "./App.css";
+import Main from "./components/MainComponent";
+import { DISHES } from "./shared/dishes";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        dishes: DISHES
-      };
-    }
-  
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
-      
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
-
-    
   }
 }
 
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,33 +1,40 @@
 import React, { Component } from "react";
+import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
 import Footer from "./FooterComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
 
-  onDishSelect(dishId) {
-    console.log(dishId + " was selected!");
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
+
+    const HomePage = () => {
+        return (
+            <Home />
+        );
+    };
+
     return (
       <div>
-      <Header />
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-          selectedDish={this.state.selectedDish}
-        />
-    <Footer />
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
