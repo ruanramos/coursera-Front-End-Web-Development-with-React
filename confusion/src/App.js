a4d39fd55d1ea2a5e2159fd8accaf13f72465246
Ruan Ramos <ruan.ramos28@gmail.com>
2020-07-11 02:24:35 -0300

    Add new home page and implement SPA

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./components/MainComponent";
-import { DISHES } from "./shared/dishes";
 import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -3,7 +3,11 @@ import Home from "./HomeComponent";
 import Header from "./HeaderComponent";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import Footer from "./FooterComponent";
+import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -11,15 +15,21 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
-
     const HomePage = () => {
-        return (
-            <Home />
-        );
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
+      );
     };
 
     return (
@@ -32,6 +42,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
