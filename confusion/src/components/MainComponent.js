9202802c618a9a19d9b3cd46028afe6c75f82d8e
Ruan Ramos <ruan.ramos@intelie.com.br>
2020-08-13 04:32:00 -0300

    Remove LocalForm and use Form with stored state

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import Dishdetail from "./DishdetailComponent";
 import { connect } from "react-redux";
 import { addComment, fetchDishes } from "../redux/ActionCreators";
+import { actions } from 'react-redux-form';
 
 
 const mapStateToProps = (state) => {
@@ -24,7 +25,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
   // I need to map the methods here so they are available for my Main component to nake use of
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes()) }
+  fetchDishes: () => { dispatch(fetchDishes()) },
+  resetFeedbackForm: () => dispatch(actions.reset('feedback'))
 });
 
 class Main extends Component {
@@ -77,7 +79,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.props.dishes} />}
           />
-          <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
           <Route
             exact
             path="/aboutus"
